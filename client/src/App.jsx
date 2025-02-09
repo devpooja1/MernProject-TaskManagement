3f849d0f342f1c7a0d11f2948573a27f3a525f8f
Pooja <poojarajput020@gmail.com>
2025-02-09 20:19:35 +0530

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import AssignTask from "./pages/AssignTask";
 import DisplayEmpTask from "./pages/DisplayEmpTask";
 import EmpDashboard from "./pages/EmpDashboard";
 import EmpReport from "./pages/EmpReport";
-import ChangePassword from "./pages/changePassword";
+import ChangePassword from "./pages/ChangePassword";
+
 
 
 const App=()=>{
--- server/controllers/empController.js
+++ server/controllers/empController.js
@@ -40,29 +40,7 @@ const empTaskSubmit =async(req,res)=>{
         console.log(error);
     }
 }
-// const chnagePassword = async(req,res)=>{
-//     const { oldPassword, newPassword } = req.body;
 
-//     if (!oldPassword || !newPassword) {
-//         return res.status(400).json({ message: "All fields are required" });
-//     }
-
-//     try {
-//         const employee = await EmpModel.findOne({ password: currentPassword });
-//         if (!employee) {
-//             return res.status(400).json({ message: "Current password is incorrect" });
-//         }
-
-//         // Updating password without hashing
-//         employee.password = newPassword;
-//         await employee.save();
-
-//         res.status(200).json({ message: "Password changed successfully" });
-//     } catch (error) {
-//         console.error("Error changing password:", error);
-//         res.status(500).json({ message: "Server error", error });
-//     }
-// }
 const passwordChange=async(req,res)=>{
     const {oldpassword,newpassword,empid}=req.body;
  try {
